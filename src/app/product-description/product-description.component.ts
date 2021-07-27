83f72e52c012c41844dfb1f5899ba1283f234f3b
sivareddysp <sivareddy.sp590@gmail.com>
2021-07-27 17:59:53 +0530

    'Angular-AlbumStoreProductPage'

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
-  albumInfo;
+  albumInfo: () => any;
 
   constructor(private _productService: ProductService) { }
 
