db3384c967256a6f6f1b9695f21ab3e96524f3db
sivareddysp <sivareddy.sp590@gmail.com>
2021-07-28 11:07:35 +0530

    'Angular-AlbumStoreProductPage'

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ProductService } from 'app/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
